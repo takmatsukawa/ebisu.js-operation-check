16702dc3ca3c8716ae55eaa779e3adc53606f37b
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-05-04 13:32:00 +0900

    Detailed description

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <main class="container">
-    <h1>Ebisu.js</h1>
+    <h1>How to use Ebisu.js</h1>
     <p>
-      <a href="https://fasiha.github.io/ebisu.js/"
-        >https://fasiha.github.io/ebisu.js/</a
-      >
+      <a href="https://fasiha.github.io/ebisu.js/">Ebisu.js</a> is a JavaScript
+      library to deal with forgetting curve.
     </p>
     <pre><code>import {defaultModel, predictRecall, updateRecall} from 'ebisu-js';
+// <a href="#log0">t=0</a> Initialize the model
 let model = defaultModel({{halfLife}});
-// After 1t
+// After <a href="#log1">t=1</a>
 console.log(predictRecall(model, 1, true));
 // ...
-// After 11t, 1 success of 1 trial
+// After <a href="#log11">t=11</a>, 1 success of 1 trial
 model = updateRecall(model, 1, 1 11);
-// After 1t since last `updateRecall`
-console.log(predictRecall(model, 1, true));
+// ...
+// After <a href="#log21">t=10</a> since last `updateRecall`, unsuccess of 1 trial
+model = updateRecall(model, 0, 1 10);
 </code></pre>
     <p>
       <label for="halfLife"
@@ -33,11 +34,15 @@ console.log(predictRecall(model, 1, true));
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(log, i) in logs" :key="i">
+        <tr v-for="(log, i) in logs" :key="i" :id="`log${i}`">
           <td>{{ log[0] }}</td>
           <td>{{ log[1] }}</td>
-          <td :style="{ backgroundColor: log[2] > 0 ? '#ff572247' : '' }">{{ log[2] }}</td>
-          <td :style="{ backgroundColor: log[3] > 0 ? '#ff572247' : '' }">{{ log[3] }}</td>
+          <td :style="{ backgroundColor: log[2] > 0 ? '#ff572247' : '' }">
+            {{ log[2] }}
+          </td>
+          <td :style="{ backgroundColor: log[3] > 0 ? '#ff572247' : '' }">
+            {{ log[3] }}
+          </td>
           <td>{{ log[4] }}</td>
         </tr>
       </tbody>
